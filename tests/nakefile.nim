83bd24e72123a2716a6eeeb129b3b12593c58e34
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2015-10-21 16:39:58 +0300

    Added -d:ssl flag to tests

--- tests/nakefile.nim
+++ tests/nakefile.nim
@@ -4,4 +4,4 @@ task defaultTask, "Build and run":
     for nimFile in walkFiles "*.nim":
         if nimFile != "nakefile.nim":
             echo "Running: ", nimFile
-            direShell nimExe, "c", "--run", nimFile
+            direShell nimExe, "c", "--run", "-d:ssl", nimFile
